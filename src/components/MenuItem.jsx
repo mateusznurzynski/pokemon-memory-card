dc25409fd2c18ed4a864ac84a1e0b2d9896c2036
Mateusz Nurzyński <42718924+mateusznurzynski@users.noreply.github.com>
2023-12-26 14:46:42 +0100

    Style: Add GitHub icon to 'GitHub Repo' menu item

--- src/components/MenuItem.jsx
+++ src/components/MenuItem.jsx
@@ -1,5 +1,12 @@
-function MenuItem({text, onClick}) {
-	return ( <div onClick={onClick} className="menu-item">{text}</div> );
+function MenuItem({ text, onClick, iconElement }) {
+  return (
+    <div onClick={onClick} className='menu-item'>
+      <div className='menu-item-wrapper'>
+        {iconElement || null}
+        <div className='menu-item-text'>{text}</div>
+      </div>
+    </div>
+  )
 }
 
-export default MenuItem;
+export default MenuItem
